1b83234dfb3eac6c6cba6e211f8b2a00fc634378
jorgeL3ao <jorgesalgado1190@gmail.com>
2023-03-06 11:01:33 -0300

    Adicionando funcionalidade

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  attr_accessor :bio, :email, :full_name, :location, :password, :password_confirmation
+
+  validates_presence_of :email, :full_name, :location, :password
+  validates_confirmation_of :password
+  validates_length_of :bio, :minimum => 30, :allow_blank => false
+
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates_uniqueness_of :email
+end
